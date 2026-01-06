b848eae300b5a2904fe129ac686c0dc57b1a8a62
Dadu Khan <photoshopt65@gmail.com>
2026-01-06 12:07:13 +0500

    Add Clerk integration with webhook handling and update dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebHooks from "./controllers/clerkWebHooks.js";
 
 connectDB();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+// api to listen clerk webhooks
+app.use("/api/clerk", clerkWebHooks);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
