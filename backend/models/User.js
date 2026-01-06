12696b828d83269fa61ec33eff30a128949d8843
Dadu Khan <photoshopt65@gmail.com>
2026-01-06 11:13:24 +0500

    Add User model with schema definition for user data

---
+++ backend/models/User.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    image: { type: String, required: true },
+    role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
+    recentSearchCities: [{ type: String, required: true }],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
